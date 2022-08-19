9ceeceb22f6e64b1f6042486767daf0a1c38b72c
Zaidibeth Ramos <38891128+zergcore@users.noreply.github.com>
2022-08-19 08:10:29 -0400

    CSS, SASS class

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import '../styles/Login.scss';
 const Login = () => {
     return (
         <div className="login">
@@ -8,15 +8,14 @@ const Login = () => {
                 <h1 className="title">Create a new password</h1>
                 <p className="subtitle">Enter a new passwrd for yue account</p>
                 <form action="/" className="form">
-                <label htmlFor="password" className="label">Password</label>
-                <input type="password" id="password" placeholder="*********" className="input input-password" />
-                <label htmlFor="new-password" className="label">Password</label>
-                <input type="password" id="new-password" placeholder="*********" className="input input-password" />
-                <input type="submit" defaultValue="Confirm" className="primary-button login-button" />
+                    <label htmlFor="password" className="label">Password</label>
+                    <input type="password" id="password" placeholder="*********" className="input input-password" />
+                    <label htmlFor="new-password" className="label">Password</label>
+                    <input type="password" id="new-password" placeholder="*********" className="input input-password" />
+                    <input type="submit" defaultValue="Confirm" className="primary-button login-button" />
                 </form>
             </div>
-            </div>
-
+        </div>
     );
 }
 
