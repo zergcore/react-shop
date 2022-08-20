f0d1e46897bfe2681be55163fbc2aaef327f87ab
Zaidibeth Ramos <38891128+zergcore@users.noreply.github.com>
2022-08-19 22:35:20 -0400

    Rendering info from api

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -4,18 +4,18 @@ import '@styles/ProductItem.scss';
 import addToCart from "@icons/bt_add_to_cart.svg";
 const image = "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940";
 
-const ProductItem = () => {
+const ProductItem = ({product}) => {
 	const [cart, setCart] = useState([]);
 	const handleClick = () =>{
 		setCart([]);
 	}
 	return (
 		<div className="ProductItem">
-			<img src={image} alt="" />
+			<img src={product.images[0]} alt="" />
 			<div className="product-info">
 				<div>
-					<p>$120,00</p>
-					<p>Bike</p>
+					<p>${product.price}</p>
+					<p>{product.title}</p>
 				</div>
 				<figure onClick={handleClick}>
 					<img src={addToCart} alt="" />
---
+++ src/hooks/useGetProducts.js
@@ -0,0 +1,16 @@
+import { useEffect,useState } from "react";
+import axios from 'axios';
+
+const useGetProducts = (API) => {
+    const [products, setProducts]=useState([]);
+	useEffect(() => {
+        async function fetchData() {
+            const response = await axios.get(API); //request
+            setProducts(response.data);
+        }
+        fetchData();
+    }, []);
+  return products;
+}
+
+export default useGetProducts
